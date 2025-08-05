a4bf7226c44a31c1c5ff160c705be4cb8e514f2b
YISUSBG <jesusbersabegonzalez@gmail.com>
2025-08-05 21:26:58 +0200

    Seventh Commit Config Capa presentacion

--- src/main/java/gm/estudiantes/EstudiantesApplication.java
+++ src/main/java/gm/estudiantes/EstudiantesApplication.java
@@ -1,14 +1,35 @@
 package gm.estudiantes;
 
+import gm.estudiantes.servicio.EstudianteServicio;
+import lombok.extern.flogger.Flogger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner {
+
+	@Autowired
+	private EstudianteServicio estudianteServicio;
+
+	private static final Logger logger =
+			LoggerFactory.getLogger(EstudiantesApplication.class);
+
+	String nl = System.lineSeparator();
 
 	public static void main(String[] args) {
-
+		logger.info("Iniciando la aplicacion...");
+		// Levantar la fabrica de Spring
 		SpringApplication.run(EstudiantesApplication.class, args);
+		logger.info("Aplicacion finalizada!");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info(nl + "Ejecutando metodo run de Spring..." + nl);
+
+	}
 }
